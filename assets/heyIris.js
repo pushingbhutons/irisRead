11d6f8113b1009ea311f61c3da66e64a40eb875e
Finley <altimagewritingco@gmail.com>
2018-09-27 04:20:53 +0200

    Update heyIris.js
    build "heyIris" hotword comment outline

--- assets/heyIris.js
+++ assets/heyIris.js
@@ -1,1 +1,19 @@
-
+// imports
+// hotword (on & wait / off ) 
+//     hotword = "heyIris" (voice recognition??)
+// security (no recording?)
+//     hotword = "heyIris" (unrecognized voice triggers "heyAbbot" guest controls??)
+// dialog manager (any commands required, behind the scenes, check docs again)
+// intent list (words Iris recognizes and reacts to)
+//     "Hey Iris, who is captain dante?"
+//     Triggers keyword / label search for "captain dante" using https://book1.pushingbhutons.com & https://book2.pushingbhutons.com as source
+// intent training library (call actions - Iris actions)
+//     Iris scans authorized sources for answer to question
+// replyIris (she's gotta answer or there's no damn point in this)
+//     iris replies with answer (she doesn't care if you like it)
+// resetIris (a good AI asks for more)
+//     iris asks if you've got other questions or if you want her to chill out 
+// error logging (default?)
+//     random bugs call for random messages of hopeAI (inspirobot quotes "readAloud" as 404 / 503 / error)
+// testing
+//     iris wont act the same in production as in the wild. Learn to deal with her quirky sense of humor now, before she says some thing she learned from inspirobot.
